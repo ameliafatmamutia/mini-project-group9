9d1eecd6372b9847c890ba75d11f5d3fa8425fe9
AMELIA FATMA MUTIA <ameliafatmam@gmail.com>
2023-04-16 12:27:29 +0700

    edit frontend add login and registration

---
+++ frontend/src/pages/Auth/Register.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import "../../assets/styles/RegisterPage.css";
+import Axios from "axios";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    phone_number: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await Axios.post("http://localhost:8000/auth", formData);
+      console.log(response.data); // Handle response data as needed
+    } catch (error) {
+      console.error("Error registering:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <br />
+      <Form className="my-form-control form-container" onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPhoneNumber">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control
+            type="tel"
+            placeholder="Enter phone number"
+            name="phone_number"
+            value={formData.phone_number}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <br />
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(e) => handleSubmit(e)}
+        >
+          Register
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Register;
